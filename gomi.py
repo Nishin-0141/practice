9d2f4bd32f5ede1958ae8c3e46f99752130cd02d
Nishin-0141 <Nishin-0141.com>
2021-02-14 03:17:27 +0900

    third

--- gomi.py
+++ gomi.py
@@ -1,9 +1,1 @@
-n = int(input())
-a = list(map(int, input().split()))
-b = [0]*(n+1)
-for i in range(1, n+1):
-    b[i - 1] = a[i] * i
-ans = ''
-for i in range(len(b)):
-    ans += str(b[i]) + ' '
-print(ans)
+print(1)
